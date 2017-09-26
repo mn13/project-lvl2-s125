5b3f68a4b10a75a7556259c346b9728063dfd341
nma <mndvl0p@gmail.com>
2017-09-26 16:53:40 +0300

    add tests for deep compare

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -20,3 +20,24 @@ test('ini', () => {
   const iniDiff = genDiff(pathToFile1, pathToFile2);
   expect(iniDiff).toEqual(expected);
 });
+
+const expectedAST = '{\n    common: {\n        setting1: Value 1\n      - setting2: 200\n        setting3: true\n      - setting6: {\n            key: value\n        }\n      + setting4: blah blah\n      + setting5: {\n            key5: value5\n        }\n    }\n    group1: {\n      + baz: bars\n      - baz: bas\n        foo: bar\n    }\n  - group2: {\n        abc: 12345\n    }\n  + group3: {\n        fee: 100500\n    }\n}';
+
+test('jsonAST', () => {
+  const pathToFile1 = `${__dirname}/json/beforeAST.json`;
+  const pathToFile2 = `${__dirname}/json/afterAST.json`;
+  const jsonDiff = genDiff(pathToFile1, pathToFile2);
+  expect(jsonDiff).toEqual(expectedAST);
+});
+test('yamlAST', () => {
+  const pathToFile1 = `${__dirname}/yaml/beforeAST.yaml`;
+  const pathToFile2 = `${__dirname}/yaml/afterAST.yaml`;
+  const yamlDiff = genDiff(pathToFile1, pathToFile2);
+  expect(yamlDiff).toEqual(expectedAST);
+});
+test('iniAST', () => {
+  const pathToFile1 = `${__dirname}/ini/beforeAST.ini`;
+  const pathToFile2 = `${__dirname}/ini/afterAST.ini`;
+  const iniDiff = genDiff(pathToFile1, pathToFile2);
+  expect(iniDiff).toEqual(expectedAST);
+});
