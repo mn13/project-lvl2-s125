5f6769148b4a696d739f312cf7bb7ebefe8ae6f3
nma <mndvl0p@gmail.com>
2017-09-29 16:11:01 +0300

    change output format for deep diff's

--- src/genDiffLib.js
+++ src/genDiffLib.js
@@ -6,7 +6,7 @@ import parse from './parse';
 const getContent = pathToFile => parse(fs.readFileSync(pathToFile, 'utf-8'), path.extname(pathToFile));
 
 const status = {
-  saved: '  ', added: '+ ', removed: '- ', changed: '+- ',
+  saved: ' ', added: '+', removed: '-', changed: '+-',
 };
 
 const propertyActions = [
@@ -23,12 +23,18 @@ const propertyActions = [
   {
     check: (prop1, prop2) => prop2 === undefined,
     getStatus: status.removed,
-    getProperty: _.identity,
+    getProperty: (prop1, prop2, f) => {
+      if (prop1 instanceof Object) return f(prop1, prop1);
+      return prop1;
+    },
   },
   {
     check: prop1 => prop1 === undefined,
     getStatus: status.added,
-    getProperty: (prop1, prop2) => prop2,
+    getProperty: (prop1, prop2, f) => {
+      if (prop2 instanceof Object) return f(prop2, prop2);
+      return prop2;
+    },
   },
   {
     check: (prop1, prop2) => prop1 !== prop2,
@@ -45,19 +51,28 @@ const genDiff = (content1, content2) => {
   const diff = union
     .reduce((acc, key) => {
       const { getStatus, getProperty } = getPropertyAction(content1[key], content2[key]);
-      const item = getStatus === status.changed ? { [`${status.added}${key}`]: content2[key], [`${status.removed}${key}`]: content1[key] } :
-        { [`${getStatus}${key}`]: getProperty(content1[key], content2[key], genDiff) };
+      const item = getStatus === status.changed ? { [`${status.added} ${key}`]: content2[key], [`${status.removed} ${key}`]: content1[key] } :
+        { [`${getStatus} ${key}`]: getProperty(content1[key], content2[key], genDiff) };
       return { ...acc, ...item };
     }, {});
-  // console.log(diff);
   return diff;
 };
-const formatDiff = diff => JSON.stringify(diff, null, 2)
-  .replace(/,/g, '')
-  .replace(/"/g, '');
+const format = (diff) => {
+  const next = indent => indent + 4;
+
+  const iter = (d, indent) => {
+    const res = Object.keys(d)
+      .reduce((acc, key) => {
+        const property = d[key] instanceof Object ? iter(d[key], next(indent)) : d[key];
+        return `${acc}${' '.repeat(indent)}${key}: ${property}\n`;
+      }, '');
+    return `{\n${res}${' '.repeat(indent - 2)}}`;
+  };
+  return iter(diff, 2);
+};
 
 
 export default (pathToFile1, pathToFile2) => {
   const diff = genDiff(getContent(pathToFile1), getContent(pathToFile2));
-  return formatDiff(diff);
+  return format(diff);
 };
