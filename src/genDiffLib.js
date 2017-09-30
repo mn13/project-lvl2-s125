27538ff6d5f5477c4fa8bdced44815900ae061d3
nma <mndvl0p@gmail.com>
2017-09-30 16:22:25 +0300

    refactor

--- src/genDiffLib.js
+++ src/genDiffLib.js
@@ -5,43 +5,42 @@ import parse from './parse';
 
 const getContent = pathToFile => parse(fs.readFileSync(pathToFile, 'utf-8'), path.extname(pathToFile));
 
-const status = {
-  saved: ' ', added: '+', removed: '-', changed: '+-',
-};
-
 const propertyActions = [
   {
-    check: (prop1, prop2) => prop1 === prop2,
-    getStatus: status.saved,
+    check: (before, after) => before === after,
+    type: 'saved',
+    getChildren: () => [],
     getProperty: _.identity,
   },
   {
-    check: (prop1, prop2) => prop1 instanceof Object && prop2 instanceof Object,
-    getStatus: status.saved,
-    getProperty: (prop1, prop2, f) => f(prop1, prop2),
+    check: (before, after) => before instanceof Object && after instanceof Object,
+    type: 'saved',
+    getChildren: (before, after, f) => f(before, after),
+    getProperty: (before, after) => ({ before, after }),
   },
   {
-    check: (prop1, prop2) => prop2 === undefined,
-    getStatus: status.removed,
-    getProperty: (prop1, prop2, f) => {
-      if (prop1 instanceof Object) return f(prop1, prop1);
-      return prop1;
-    },
+    check: (before, after) => after === undefined,
+    type: 'removed',
+    getChildren: () => [],
+    getProperty: _.identity,
   },
   {
-    check: prop1 => prop1 === undefined,
-    getStatus: status.added,
-    getProperty: (prop1, prop2, f) => {
-      if (prop2 instanceof Object) return f(prop2, prop2);
-      return prop2;
-    },
+    check: before => before === undefined,
+    type: 'added',
+    getChildren: () => [],
+    getProperty: (before, after) => _.identity(after),
   },
   {
-    check: (prop1, prop2) => prop1 !== prop2,
-    getStatus: status.changed,
-    getProperty: (prop1, prop2) => [prop1, prop2],
+    check: (before, after) => before !== after,
+    type: 'updated',
+    getChildren: () => [],
+    getProperty: (before, after) => ({ before, after }),
   },
 ];
+
+const buildNode = (name, property, type, children) => ({
+  name, property, type, children,
+});
 
 const getPropertyAction = (arg1, arg2) => _.find(propertyActions, ({ check }) => check(arg1, arg2));
 
@@ -49,30 +48,48 @@ const genDiff = (content1, content2) => {
   const union = _.union(Object.keys(content1), Object.keys(content2));
 
   const diff = union
-    .reduce((acc, key) => {
-      const { getStatus, getProperty } = getPropertyAction(content1[key], content2[key]);
-      const item = getStatus === status.changed ? { [`${status.added} ${key}`]: content2[key], [`${status.removed} ${key}`]: content1[key] } :
-        { [`${getStatus} ${key}`]: getProperty(content1[key], content2[key], genDiff) };
-      return { ...acc, ...item };
+    .map((key) => {
+      const {
+        type, getProperty, getChildren,
+      } = getPropertyAction(content1[key], content2[key]);
+      return buildNode(
+        key, getProperty(content1[key], content2[key]),
+        type, getChildren(content1[key], content2[key], genDiff),
+      );
     }, {});
+  // console.log(diff);
   return diff;
 };
-const format = (diff) => {
-  const next = indent => indent + 4;
 
-  const iter = (d, indent) => {
-    const res = Object.keys(d)
-      .reduce((acc, key) => {
-        const property = d[key] instanceof Object ? iter(d[key], next(indent)) : d[key];
-        return `${acc}${' '.repeat(indent)}${key}: ${property}\n`;
-      }, '');
-    return `{\n${res}${' '.repeat(indent - 2)}}`;
-  };
-  return iter(diff, 2);
+const render = (diff) => {
+  const signs = { saved: ' ', added: '+', removed: '-' };
+
+  const getLine = (indent, sign, name, prop) => `${' '.repeat(indent)}${sign} ${name}: ${prop}`;
+
+  const lines = diff.map((item) => {
+    const {
+      name, property, type, children,
+    } = item;
+
+    if (children.length > 0) {
+      return getLine(2, signs[type], name, render(children));
+    }
+    if (type === 'updated') {
+      return `${getLine(2, signs.added, name, property.after)}\n${getLine(2, signs.removed, name, property.before)}`;
+    }
+    if (property instanceof Object) {
+      const parsedProp = Object.keys(property)
+        .map(key => getLine(2, signs.saved, key, property[key]));
+      return getLine(2, signs[type], name, `{\n${parsedProp.join('\n')}\n${' '.repeat(2)}}`);
+    }
+
+    return getLine(2, signs[type], name, property);
+  });
+  return `{\n${lines.join('\n')}\n}`;
 };
 
 
 export default (pathToFile1, pathToFile2) => {
   const diff = genDiff(getContent(pathToFile1), getContent(pathToFile2));
-  return format(diff);
+  return render(diff);
 };
