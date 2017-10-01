a36c4a6e43c83c4949b5bdedf2db3a04614b51cc
nma <mndvl0p@gmail.com>
2017-10-01 11:29:53 +0300

    add plain output

--- src/genDiffLib.js
+++ src/genDiffLib.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 import parse from './parse';
+import render from './render';
 
 const getContent = pathToFile => parse(fs.readFileSync(pathToFile, 'utf-8'), path.extname(pathToFile));
 
@@ -60,37 +61,7 @@ const genDiff = (content1, content2) => {
   return diff;
 };
 
-const render = (diff) => {
-  const signs = { saved: ' ', added: '+', removed: '-' };
-
-  const getLine = (indent, sign, name, prop) => `${' '.repeat(indent)}${sign} ${name}: ${prop}`;
-  const iter = (d, indent) => {
-    const lines = d.map((item) => {
-      const {
-        name, property, type, children,
-      } = item;
-
-      if (children.length > 0) {
-        return getLine(indent, signs[type], name, iter(children, indent + 4));
-      }
-      if (type === 'updated') {
-        return `${getLine(indent, signs.added, name, property.after)}\n${getLine(indent, signs.removed, name, property.before)}`;
-      }
-      if (property instanceof Object) {
-        const parsedProp = Object.keys(property)
-          .map(key => getLine(indent + 4, signs.saved, key, property[key]));
-        return getLine(indent, signs[type], name, `{\n${parsedProp.join('\n')}\n${' '.repeat(indent + 2)}}`);
-      }
-
-      return getLine(indent, signs[type], name, property);
-    });
-    return `{\n${lines.join('\n')}\n${' '.repeat(indent - 2)}}`;
-  };
-  return iter(diff, 2);
-};
-
-
-export default (pathToFile1, pathToFile2) => {
+export default (pathToFile1, pathToFile2, format = 'default') => {
   const diff = genDiff(getContent(pathToFile1), getContent(pathToFile2));
-  return render(diff);
+  return render[format](diff);
 };
